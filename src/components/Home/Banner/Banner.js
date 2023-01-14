b55619cdcc8b9e4ad59508fd85875f8dca62dbda
IffatNur <iffatnoorshad00@gmail.com>
2023-01-14 16:03:47 +0600

    footer updated

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -8,18 +8,27 @@ import './Banner.css';
 
 const Banner = () => {
     return (
-        <div>
-            <div className='bg-gradient-to-tl from-sky-900 to-green-900 h-96 w-full relative'>
-                <img className='w-full h-full object-cover absolute mix-blend-overlay' src={backgroundPic} alt='' />
-                <div className='p-24 text-center'>
-                    <h1 className='text-white text-5xl font-bold mb-8'>Let’s Help Those Who Are More In Need</h1>
-                    <h2 className='text-slate-300 text-3xl font-light hidden lg:block'>Lorem ipsum dolor sit amet, usu cu esse possit referrentur, Lorem ipsum dolor sit amet, usu cu esse possit referrentur</h2>
-                    <a href='/donate'><button className='btn btn-primary mt-4'>Donate Now</button></a>
-                </div>
-
-            </div>
+      <div>
+        <div className="bg-gradient-to-tl from-sky-900 to-green-900 h-96 w-full relative">
+          <img
+            className="w-full h-full object-cover absolute mix-blend-overlay"
+            src={backgroundPic}
+            alt=""
+          />
+          <div className="p-24 text-center">
+            <h1 className="text-white text-5xl font-bold mb-8">
+              No kid should be left hungry
+            </h1>
+            <h2 className="text-slate-300 text-3xl font-light hidden lg:block">
+              Lorem ipsum dolor sit amet, usu cu esse possit referrentur, Lorem
+              ipsum dolor sit amet, usu cu esse possit referrentur
+            </h2>
+            <a href="/donate">
+              <button className="btn btn-primary mt-4">Donate Now</button>
+            </a>
+          </div>
         </div>
-
+      </div>
     );
 };
 
